64e77c33b96003c2f65fdf6d9756eeac228eed12
Marina Serafeimidi <M.Serafeimidi@student.tudelft.nl>
2022-12-08 16:53:06 +0100

    add necessary endpoints

--- customer-microservice/src/main/java/nl/tudelft/sem/template/example/Customer.java
+++ customer-microservice/src/main/java/nl/tudelft/sem/template/example/Customer.java
@@ -1,12 +1,20 @@
 package nl.tudelft.sem.template.example;
 
-import java.util.ArrayList;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.List;
 
+@Entity
 public class Customer {
-
+    @Column
+    @ElementCollection
     List<String> usedCoupons;
+    @Column
+    @ElementCollection
     List<String> allergens;
+    @Id
     String customerId;
 
     /** Constructor for Customer object.
@@ -21,4 +29,19 @@ public class Customer {
         this.customerId = customerId;
     }
 
+    public List<String> getUsedCoupons() {
+        return usedCoupons;
+    }
+
+    public void setUsedCoupons(List<String> usedCoupons) {
+        this.usedCoupons = usedCoupons;
+    }
+
+    public List<String> getAllergens() {
+        return allergens;
+    }
+
+    public void setAllergens(List<String> allergens) {
+        this.allergens = allergens;
+    }
 }
--- customer-microservice/src/main/java/nl/tudelft/sem/template/example/CustomerRepository.java
+++ customer-microservice/src/main/java/nl/tudelft/sem/template/example/CustomerRepository.java
@@ -1,7 +1,10 @@
 package nl.tudelft.sem.template.example;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, String> {
+
 
 }
