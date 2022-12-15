1a577120b384ca22885c3d170a805470d66f59f7
Nicolae Filat <nicolae.filat@gmail.com>
2022-12-15 09:36:10 +0100

    Reverted changed for example microservice and changed coupon microservice to have the right pipeline

--- example-microservice/src/main/java/nl/tudelft/sem/template/example/Application.java
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/Application.java
@@ -2,13 +2,11 @@ package nl.tudelft.sem.template.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Example microservice application.
  */
 @SpringBootApplication
-@ComponentScan({"nl.tudelft.sem.template.authentication", "nl.tudelft.sem.template.example"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
