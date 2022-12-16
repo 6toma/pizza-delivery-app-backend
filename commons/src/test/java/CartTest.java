d2ffe076cb0033161517480a97cb46048f4cba1c
Jarno Berger <j.berger@student.tudelft.nl>
2022-12-16 14:10:24 +0100

    3 basic tests for commons entities
    Tested all methods from commons entities

---
+++ commons/src/test/java/CartTest.java
@@ -0,0 +1,47 @@
+import nl.tudelft.sem.template.authentication.NetId;
+import nl.tudelft.sem.template.commons.entity.Cart;
+import nl.tudelft.sem.template.commons.entity.Pizza;
+import nl.tudelft.sem.template.commons.entity.Topping;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CartTest {
+
+    private Topping t1 = new Topping("pineapple", 1.5);
+    private Topping t2 = new Topping("salami", 1.5);
+    private Pizza p1 = new Pizza("hawaii", List.of(t1), 7);
+    private Cart c1 = new Cart(new NetId("me"), new ArrayList<>());
+
+
+    @Test
+    public void addPizzaTest() {
+        c1.addPizza(p1);
+        assertEquals(c1.getPizzas().size(), 1);
+    }
+
+    public void removePizzaTest() {
+        c1.addPizza(p1);
+        c1.removePizza(p1);
+        assertEquals(c1.getPizzas().size(), 0);
+    }
+
+    public void addToppingTest() {
+        c1.addPizza(p1);
+        c1.addTopping(p1, t2);
+        assertEquals(c1.getPizzas().get(0).getPrice(), 8.5);
+        assertEquals(c1.getPizzas().get(0).getToppings().size(), 2);
+        assertEquals(c1.getPizzas().size(), 1);
+    }
+
+    public void removeToppingTest() {
+        c1.addPizza(p1);
+        c1.removeTopping(p1, t1);
+        assertEquals(c1.getPizzas().get(0).getPrice(), 5.5);
+        assertEquals(c1.getPizzas().get(0).getToppings().size(), 2);
+        assertEquals(c1.getPizzas().size(), 1);
+    }
+}
---
+++ commons/src/test/java/PizzaTest.java
@@ -0,0 +1,70 @@
+import nl.tudelft.sem.template.commons.entity.Pizza;
+import nl.tudelft.sem.template.commons.entity.Topping;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PizzaTest {
+
+    private Topping t1 = new Topping("pineapple", 1.5);
+    private Topping t2 = new Topping("salami", 1.5);
+    private Pizza p1 = new Pizza("hawaii", List.of(t1), 7);
+    private Pizza p2 = new Pizza("hawaii", List.of(t1), 7);
+    private Pizza p3 = new Pizza("american", List.of(t1, t2), 8.5);
+
+
+    @Test
+    public void getNameTest() {
+        assertEquals(p1.getPizzaName(), "hawaii");
+    }
+
+    @Test
+    public void getPriceTest() {
+        assertEquals(p1.getPrice(), 7);
+    }
+
+    @Test
+    public void getToppingsTest() {
+        assertEquals(p1.getToppings(), List.of(t1));
+    }
+
+    @Test
+    public void toStringTest() {
+        assertEquals(p3.toString(), "american;pineapple 1.5;salami 1.5;8.5");
+    }
+
+    @Test
+    public void hashCodeTest() {
+        assertEquals(p1.hashCode(), p2.hashCode());
+        assertNotEquals(p1.hashCode(), p3.hashCode());
+    }
+
+    @Test
+    public void testEquals() {
+        assertTrue(p1.equals(p2));
+        assertTrue(!p1.equals(p3));
+    }
+
+    @Test
+    public void testAddTopping() {
+        List<Topping> list = new ArrayList<>();
+        list.add(t1);
+        Pizza temp = new Pizza("hawaii", list, 7);
+        temp.addTopping(t2);
+        assertTrue(temp.getToppings().size() == 2);
+        assertTrue(temp.getPrice() == 8.5);
+    }
+
+    @Test
+    public void testRemoveTopping() {
+        List<Topping> list = new ArrayList<>();
+        list.add(t1);
+        Pizza temp = new Pizza("hawaii", list, 7);
+        temp.removeTopping(t1);
+        assertTrue(temp.getToppings().size() == 0);
+        assertTrue(temp.getPrice() == 5.5);
+    }
+}
