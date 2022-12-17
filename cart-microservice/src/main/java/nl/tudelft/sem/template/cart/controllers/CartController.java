f76cbf4793fb421d01d0c3165b213e3d0bdfda0c
Jarno Berger <j.berger@student.tudelft.nl>
2022-12-17 21:33:58 +0100

    Added getCart function
    Created a function so checkout can get the cart by netID

--- cart-microservice/src/main/java/nl/tudelft/sem/template/cart/controllers/CartController.java
+++ cart-microservice/src/main/java/nl/tudelft/sem/template/cart/controllers/CartController.java
@@ -15,10 +15,7 @@ import nl.tudelft.sem.template.commons.entity.Topping;
 import nl.tudelft.sem.template.commons.utils.RequestHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cart")
@@ -104,6 +101,11 @@ public class CartController {
         cart.removeTopping(pizza, topping);
         cartRepository.save(cart);
         return "Topping was added";
+    }
+
+    @GetMapping("/getCart")
+    Cart getCart(@RequestBody NetId netId) {
+        return cartRepository.findByNetId(netId);
     }
 
 
