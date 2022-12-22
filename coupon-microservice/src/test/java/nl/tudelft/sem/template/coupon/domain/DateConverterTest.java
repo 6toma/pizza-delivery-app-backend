021eec81e71ebbb642b5463c039140467e2595f1
Angelika Mentzelopoulou <a.mentzelopoulou@student.tudelft.nl>
2022-12-22 11:47:34 +0100

    Update coupon-microservice/src/test/java/nl/tudelft/sem/template/coupon/domain/DateConverterTest.java

--- coupon-microservice/src/test/java/nl/tudelft/sem/template/coupon/domain/DateConverterTest.java
+++ coupon-microservice/src/test/java/nl/tudelft/sem/template/coupon/domain/DateConverterTest.java
@@ -1,9 +1,8 @@
 package nl.tudelft.sem.template.coupon.domain;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class DateConverterTest {
 
