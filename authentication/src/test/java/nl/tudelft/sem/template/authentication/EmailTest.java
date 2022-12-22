31010c959d37e0fdb1a631f6a1d975f2f7d0c61d
Nicolae Filat <nicolae.filat@gmail.com>
2022-12-22 23:12:20 +0100

    Added email tests and authManager test for coverage

--- authentication/src/test/java/nl/tudelft/sem/template/authentication/EmailTest.java
+++ authentication/src/test/java/nl/tudelft/sem/template/authentication/EmailTest.java
@@ -1,11 +1,31 @@
 package nl.tudelft.sem.template.authentication;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class EmailTest {
-    @Test
-    public void newEmail(){
 
-        UserEmail userEmail = new UserEmail("a.b@gmail.com");
+    public static List<String> validEmails() {
+        return List.of("emai@gmail.com", "a.b.c.d@yahoo.com", "email.1.2.3.5@hotmail.com",
+            "email_with_underscore@mail.com", "EmailWithCapitalLetter@gmai.com");
+    }
+
+    public static List<String> invalidEmails() {
+        return List.of("emailWithNoAt", "EmailWithIllegalChar$@mail.com", "EmailWithoutcom@gmail");
+    }
+
+    @MethodSource("validEmails")
+    @ParameterizedTest
+    public void checkValidEmail(String email) {
+        new UserEmail(email);
+    }
+
+    @MethodSource("invalidEmails")
+    @ParameterizedTest
+    public void checkInvalidEmail(String email) {
+        assertThatThrownBy(() -> new UserEmail(email)).isInstanceOf(EmailNotValidException.class);
     }
 }
