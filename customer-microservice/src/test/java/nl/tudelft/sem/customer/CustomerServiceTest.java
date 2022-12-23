c4d793449c13a351c7e139b21862b5922fbb74a5
Marina Serafeimidi <M.Serafeimidi@student.tudelft.nl>
2022-12-23 11:42:51 +0100

    service test

--- customer-microservice/src/test/java/nl/tudelft/sem/customer/CustomerServiceTest.java
+++ customer-microservice/src/test/java/nl/tudelft/sem/customer/CustomerServiceTest.java
@@ -1,4 +1,35 @@
 package nl.tudelft.sem.customer;
 
+import nl.tudelft.sem.customer.controllers.CustomerController;
+import nl.tudelft.sem.customer.domain.Customer;
+import nl.tudelft.sem.customer.domain.CustomerRepository;
+import nl.tudelft.sem.customer.domain.CustomerService;
+import nl.tudelft.sem.template.authentication.AuthManager;
+import nl.tudelft.sem.template.authentication.NetId;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class CustomerServiceTest {
+
+    private CustomerService customerService;
+    private AuthManager authManager;
+    private Customer customer;
+    private CustomerRepository repo;
+
+    @BeforeEach
+    public void setup() {
+        repo = Mockito.mock(CustomerRepository.class);
+        authManager = Mockito.mock(AuthManager.class);
+        customerService = new CustomerService(repo);
+
+        List<String> usedCoupons = Arrays.asList("coupon1", "coupon2");
+        List<String> allergens = Arrays.asList("peanut", "gluten");
+        int customerId = 123456;
+        NetId netId = new NetId("example123");
+
+        customer = new Customer(usedCoupons, allergens, customerId, netId);
+    }
 }
