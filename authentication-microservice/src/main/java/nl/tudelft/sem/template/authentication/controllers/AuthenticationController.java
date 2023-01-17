b25f0be0f84d6ae416e03a8a5447e5db5ba72b21
Daan Panis <dpdaan@hotmail.com>
2023-01-17 19:01:06 +0100

    Refactored AuthenticationController by moving exception handling and logging in to their own classes.

--- authentication-microservice/src/main/java/nl/tudelft/sem/template/authentication/controllers/AuthenticationController.java
+++ authentication-microservice/src/main/java/nl/tudelft/sem/template/authentication/controllers/AuthenticationController.java
@@ -1,10 +1,7 @@
 package nl.tudelft.sem.template.authentication.controllers;
 
 import lombok.RequiredArgsConstructor;
-import nl.tudelft.sem.template.authentication.JwtTokenGenerator;
-import nl.tudelft.sem.template.authentication.NetId;
-import nl.tudelft.sem.template.authentication.authentication.JwtUserDetailsService;
-import nl.tudelft.sem.template.authentication.domain.user.Password;
+import nl.tudelft.sem.template.authentication.domain.user.AuthenticationService;
 import nl.tudelft.sem.template.authentication.domain.user.RegistrationService;
 import nl.tudelft.sem.template.authentication.models.AuthenticationRequestModel;
 import nl.tudelft.sem.template.authentication.models.AuthenticationResponseModel;
@@ -12,11 +9,6 @@ import nl.tudelft.sem.template.authentication.models.RegistrationRequestModel;
 import nl.tudelft.sem.template.commons.utils.RequestHelper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,9 +18,7 @@ import org.springframework.web.server.ResponseStatusException;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
-    private final transient AuthenticationManager authenticationManager;
-    private final transient JwtTokenGenerator jwtTokenGenerator;
-    private final transient JwtUserDetailsService jwtUserDetailsService;
+    private final transient AuthenticationService authenticationService;
     private final transient RegistrationService registrationService;
     private final transient RequestHelper requestHelper;
 
@@ -40,21 +30,9 @@ public class AuthenticationController {
      * @throws Exception if the user does not exist or the password is incorrect
      */
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponseModel> authenticate(@RequestBody AuthenticationRequestModel request) {
-
-        try {
-            authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                    request.getNetId(),
-                    request.getPassword()));
-        } catch (DisabledException e) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "USER_DISABLED", e);
-        } catch (BadCredentialsException e) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "INVALID_CREDENTIALS", e);
-        }
-
-        final UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(request.getNetId());
-        final String jwtToken = jwtTokenGenerator.generateToken(userDetails);
+    public ResponseEntity<AuthenticationResponseModel> authenticate(@RequestBody AuthenticationRequestModel request)
+        throws Exception {
+        var jwtToken = authenticationService.loginUser(request.getNetId(), request.getPassword());
         return ResponseEntity.ok(new AuthenticationResponseModel(jwtToken));
     }
 
@@ -69,10 +47,8 @@ public class AuthenticationController {
     public ResponseEntity<Void> register(@RequestBody RegistrationRequestModel request) throws Exception {
 
         try {
-            NetId netId = new NetId(request.getNetId());
-            Password password = new Password(request.getPassword());
-            registrationService.registerUser(netId, password);
-            requestHelper.postRequest(8081, "/customers/add", netId.toString(), String.class);
+            registrationService.registerUser(request.getNetId(), request.getPassword());
+            requestHelper.postRequest(8081, "/customers/add", request.getNetId().toString(), String.class);
         } catch (Exception e) {
             e.printStackTrace();
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
